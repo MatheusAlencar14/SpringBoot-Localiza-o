eeee0ec2218fcf0e4635121661ca6754151e524b
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-11-07 15:47:18 -0300

    Implementando e mapeando Entity Cidade

--- src/main/java/com/dev/localizacao/LocalizacaoApplication.java
+++ src/main/java/com/dev/localizacao/LocalizacaoApplication.java
@@ -1,13 +1,18 @@
 package com.dev.localizacao;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocalizacaoApplication {
+public class LocalizacaoApplication implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Inicializado. Deu certo!");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/dev/localizacao/domain/entity/Cidade.java
@@ -0,0 +1,25 @@
+package com.dev.localizacao.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_cidade")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_cidade")
+    private Long id;
+
+    @Column(name = "nome", length = 50)
+    private String nome;
+
+    @Column(name = "habitantes")
+    private Long habitantes;
+}
