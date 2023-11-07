08d2333d19e260ec0e1cde9835bd28b053d6e8d9
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-11-07 15:29:23 -0300

    Criando projeto Spring Boot - Localização

---
+++ src/test/java/com/dev/localizacao/LocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
